a296fc41d11769196ccb7324b6c87d461c177d2a
Ravi Shankar <ravishankar2463@gmail.com>
2026-06-20 23:38:40 +0530

    ci: add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = '';
+
+if (isGithubActions) {
+  // Dynamically extract the repository name from "owner/repo"
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: basePath,
 };
 
 export default nextConfig;
